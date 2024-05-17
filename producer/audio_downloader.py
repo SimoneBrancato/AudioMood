26d59fba9bf91834ee34fa44c5bce588757ec686
Simone Brancato <simonebrancato18@gmail.com>
2024-05-17 19:12:23 +0200

    master | Implementing automatic download from YouTube

---
+++ producer/audio_downloader.py
@@ -0,0 +1,25 @@
+import os
+from pytube import YouTube
+from pydub import AudioSegment
+
+def get_youtube_audio(url, output_path='audio'):
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    # Download audio from YT
+    yt = YouTube(url)
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    audio_file = audio_stream.download(output_path)
+
+    # Convert the downloaded file to MP3
+    base, ext = os.path.splitext(audio_file)
+    mp3_file = f"{base}.mp3"
+    audio = AudioSegment.from_file(audio_file)
+    audio.export(mp3_file, format="mp3")
+
+    # Remove the original downloaded file
+    os.remove(audio_file)
+
+    print(f"Downloaded and converted to MP3: {mp3_file}")
+    return mp3_file
--- producer/producer.py
+++ producer/producer.py
@@ -1,15 +1,44 @@
 import whisper
 import requests
+import re
+from audio_downloader import get_youtube_audio
+
+def split_string(s, max_words):
+    
+    parts = re.split(r'[,.]', s)
+    parts = [part.strip() for part in parts if part.strip()]
+    
+    result = []
+    current_part = []
+    current_word_count = 0
+
+    for part in parts:
+        words = part.split()
+        for word in words:
+            if current_word_count < max_words:
+                current_part.append(word)
+                current_word_count += 1
+            else:
+                result.append(" ".join(current_part))
+                current_part = [word]
+                current_word_count = 1
+
+        if current_part:
+            result.append(" ".join(current_part))
+            current_part = []
+            current_word_count = 0
+    
+    return result
+
 
 LOGSTASH_URL = "http://logstash:9700"
+mp3_file = get_youtube_audio('https://www.youtube.com/watch?v=L6sm1_mF7E8&t=57s')
 
 print("Audio analysis in process...")
 model = whisper.load_model("tiny")
-result = model.transcribe("audio.mp3")
-print(result["text"])
+result = model.transcribe(mp3_file)
 
-
-phrases = result["text"].split(".")
+phrases = split_string(result["text"], max_words=20)
 
 for phrase in phrases:
     print("Sending to Logstash \""+phrase+"\".")
@@ -17,3 +46,7 @@ for phrase in phrases:
     'message': phrase
     }
     response = requests.post(LOGSTASH_URL, json=data)
+
+
+
+

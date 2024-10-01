0bd8b76426209667aedb1e938838c8b82d72683b
Simone Brancato <simonebrancato18@gmail.com>
2024-10-01 23:07:50 +0200

    Cleaning project

--- app/app.py
+++ app/app.py
@@ -25,10 +25,5 @@ def submit():
     
     return render_template('analysis.html', file_name=response.text, model_size=model_size)
 
-""" @app.route('/analysis_reload', methods=['POST'])
-def reload():
-    return render_template('analysis.html')
-
- """
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

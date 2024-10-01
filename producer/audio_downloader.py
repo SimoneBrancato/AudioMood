0142845158c727bb12d71f91cbff3314fdc441f5
Simone Brancato <simonebrancato18@gmail.com>
2024-10-01 23:06:19 +0200

    Implementing spark volumes and pytubefix (pytube not working anymore)

--- producer/audio_downloader.py
+++ producer/audio_downloader.py
@@ -1,25 +1,18 @@
 import os
-from pytube import YouTube
-from pydub import AudioSegment
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+import os
 
 def get_youtube_audio(url, output_path='audio'):
-    
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
+    print("Downloading from YouTube")
+
     # Download audio from YT
-    yt = YouTube(url)
-    audio_stream = yt.streams.filter(only_audio=True).first()
-    audio_file = audio_stream.download(output_path)
-
-    # Converting to MP3
-    base, ext = os.path.splitext(audio_file)
-    mp3_file = f"{base}.mp3"
-    audio = AudioSegment.from_file(audio_file)
-    audio.export(mp3_file, format="mp3")
-
-    # Removing original file
-    os.remove(audio_file)
+    yt = YouTube(url, on_progress_callback = on_progress)
+    ys = yt.streams.get_audio_only()
+    mp3_file = ys.download(mp3=True)
 
     print(f"Downloaded and converted to MP3: {mp3_file}")
     return mp3_file
